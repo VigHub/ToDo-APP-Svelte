144440e4c7b54e481b82a060641af9cc90ce2303
Gianluca Viganò <gvigan97@gmail.com>
2022-12-19 23:37:17 +0100

    Dev (#1)
    * use pnpm
    
    * some refactor on AddToDo

--- src/components/AddToDo.svelte
+++ src/components/AddToDo.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { createForm } from 'svelte-forms-lib';
-	import * as yup from 'yup';
+	import { createForm } from "svelte-forms-lib";
+	import * as yup from "yup";
 
 	export let onAdd: (title: string) => void;
 	export let disabled: boolean;
@@ -8,19 +8,18 @@
 		title: yup.string().required()
 	});
 
-	const { form, errors, state, handleSubmit, handleReset, handleChange } = createForm({
+	const { form, errors, state, handleSubmit, handleReset, handleChange, isValid } = createForm({
 		initialValues: { title: '' },
 		validationSchema: schema,
 		onSubmit: (values) => {
 			onAdd(values.title);
 			handleReset();
-		}
+		},
 	});
 </script>
 
 <form on:submit={handleSubmit}>
 	<fieldset {disabled}>
-		<!-- <div class={'input-group' + $errors.title ? 'is-invalid' : ''}> -->
 		<div class="input-group">
 			<div class="form-floating">
 				<input
@@ -29,18 +28,15 @@
 					placeholder="Inserisci il todo"
 					id="todo-input"
 					class={'form-control ' + ($errors.title ? 'is-invalid' : '')}
-					on:change={handleChange}
-					bind:value={$form.title}
+					on:keyup={handleChange}
 				/>
 				<label for="todo-input">Inserisci il todo</label>
 			</div>
 
-			<button class="btn btn-svelte" type="submit" disabled={!$state.isValid}> Aggiungi </button>
+			<button class="btn btn-svelte" type="submit" disabled={!$isValid}> Aggiungi </button>
 		</div>
-		<!-- {#if $errors.title}
-			<div class="invalid-feedback">
-				<small>{$errors.title}</small>
-			</div>
-		{/if} -->
+		{#if $errors.title}
+		<label class="text-danger" for="title">{$errors.title}</label>
+		{/if}
 	</fieldset>
 </form>
